08a9606416a2e202ff0527c90a99258cc2ef3502
AryanG211 <141266142+AryanG211@users.noreply.github.com>
2024-07-28 22:28:25 +0530

    Time: 22 ms (94.63%), Space: 12.2 MB (96.22%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,25 @@
+class Solution:
+    def searchInsert(self, nums, target):
+        left, right = 0, len(nums) - 1
+        
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return left
+
+nums = [1, 3, 5, 7, 8]
+target = 7
+sol = Solution()
+result = sol.searchInsert(nums, target)
+print(result)
+
+
+
+
+
