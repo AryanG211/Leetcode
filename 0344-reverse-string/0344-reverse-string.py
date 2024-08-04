9d3b206dc6bd840e06d6c25badd74fdf6cda5744
AryanG211 <141266142+AryanG211@users.noreply.github.com>
2024-08-04 13:56:26 +0530

    Time: 124 ms (90.48%), Space: 19.2 MB (90.23%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def reverseString(self, s):
+       rev_string = s.reverse()
+       return rev_string
+
+s = ["H","a","n","n","a","h"]
+sol = Solution()
+result = sol.reverseString(s)
+print(result)
+        
